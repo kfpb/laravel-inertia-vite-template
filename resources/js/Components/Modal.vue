104b3858dedac7fb2efd1cc51ee82644520126b4
Muhammad Esa Yusriana <mesayusriana12@gmail.com>
2023-04-05 07:21:18 +0700

    style: make modal overflow

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -13,7 +13,7 @@ const { show } = defineProps({
       leaveActiveClass="transition-all duration-50"
       enterFromClass="-translate-y-full opacity-0"
       leaveToClass="-translate-y-full opacity-0">
-      <div v-if="show" class="fixed top-0 left-0 w-full h-full min-h-screen flex justify-center backdrop-blur p-10 z-20">
+      <div v-if="show" class="fixed top-0 left-0 w-full h-full min-h-screen flex justify-center backdrop-blur p-10 z-20 overflow-auto">
         <slot />
       </div>
     </transition>
